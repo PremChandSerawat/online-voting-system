aed1b5659de98bf92330ded696c0b33caff6a2b7
PremChandSerawat <47656304+PremChandSerawat@users.noreply.github.com>
2019-05-22 13:53:37 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports={
-  //mongoUrlAdmin:'mongodb+srv://prem:Panditji6228!@cluster0-0xtyz.mongodb.net/test?retryWrites=true',
-  //mongoUrlVoter:'mongodb+srv://prem:Panditji6228!@cluster0-0xtyz.mongodb.net/voter?retryWrites=true',
-  mongoUrl:'mongodb+srv://prem:Panditji6228!@cluster0-0xtyz.mongodb.net/voter?retryWrites=true'
+  //write your mongoDb url here with password
+  mongoUrl:''
 };
